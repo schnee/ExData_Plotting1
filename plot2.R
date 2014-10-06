e0d61704c6ccf3ee2bdc2e6dc593f3d2ae468784
bschneeman <bschneeman@homeaway.com>
2014-10-06 15:52:24 -0500

    plot2 script and image

---
+++ plot2.R
@@ -0,0 +1,27 @@
+# Plot 2 Source File
+
+# assumes the existande of ./data/household_power_consumption.zip, which is downloaded from
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+households = read.csv(unz("./data/household_power_consumption.zip",
+                          "household_power_consumption.txt"), sep=";", stringsAsFactors=F)
+
+# turn chr's into POSIXlt objects
+households$timestamp = strptime(paste(households$Date, households$Time), format="%d/%m/%Y %H:%M:%S")
+
+#convert to numeric
+households$Global_active_power = as.numeric(households$Global_active_power)
+
+# filter
+start = "2007-02-01 00:00:00"
+end = "2007-02-02 23:59:59"
+start = strptime(start, format="%Y-%m-%d %H:%M:%S")
+end = strptime(end, format="%Y-%m-%d %H:%M:%S")
+households_slice = subset(households, timestamp>=start & timestamp<=end)
+
+
+# and plot
+# 480 x 480 are the defaults for hist()
+png(file="plot2.png")
+plot(households_slice$timestamp, 
+     households_slice$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.off()
